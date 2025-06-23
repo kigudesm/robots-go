61f5fa46b52ff787e82d0373311b6b04a8427b5b
Konstantin <kigudesm@yandex.ru>
2025-06-23 22:22:58 +0300

    rename success

--- data_preparation/utils/read.go
+++ data_preparation/utils/read.go
@@ -8,33 +8,33 @@ import (
     "os"
 )
 
-func ReadRequest(path string) (map[string]interface{}) {
-    // Открываем JSON файл
-    file, err := os.Open(path)
-    if err != nil {
-        log.Fatalf("Ошибка открытия файла: %v", err)
-    }
-    defer file.Close()
+// func ReadRequest(path string) (map[string]interface{}) {
+//     // Открываем JSON файл
+//     file, err := os.Open(path)
+//     if err != nil {
+//         log.Fatalf("Ошибка открытия файла: %v", err)
+//     }
+//     defer file.Close()
 
-    // Читаем содержимое файла
-    bytes, err := ioutil.ReadAll(file)
-    if err != nil {
-        log.Fatalf("Ошибка чтения файла: %v", err)
-    }
+//     // Читаем содержимое файла
+//     bytes, err := ioutil.ReadAll(file)
+//     if err != nil {
+//         log.Fatalf("Ошибка чтения файла: %v", err)
+//     }
 
-    // Объявляем переменную для неизвестной структуры
-    var data map[string]interface{}
+//     // Объявляем переменную для неизвестной структуры
+//     var data map[string]interface{}
 
-    // Десериализация в интерфейс
-    err = json.Unmarshal(bytes, &data)
-    if err != nil {
-        log.Fatalf("Ошибка разбора JSON: %v", err)
-    } else{
-        events := data["events"]
-        fmt.Println("Найдено:", events)
-    }    
+//     // Десериализация в интерфейс
+//     err = json.Unmarshal(bytes, &data)
+//     if err != nil {
+//         log.Fatalf("Ошибка разбора JSON: %v", err)
+//     } else{
+//         events := data["events"]
+//         fmt.Println("Найдено:", events)
+//     }    
 
-    return data
+//     return data
     
     // // Теперь, data содержит разобранную структуру
     // switch v := data.(type) {
