0a857441c968d539277037b533324e731afea691
Konstantin <kigudesm@yandex.ru>
2025-06-24 10:47:09 +0300

    readRequest

--- main.go
+++ main.go
@@ -2,32 +2,32 @@ package main
 
 import (
 
-    // "fmt"        
-    "github.com/kigudesm/robots-go/data_preparation/utils"
+	// "fmt"
+	"github.com/kigudesm/robots-go/data_preparation/utils"
 )
 
 func main() {
-    path := "Input_samples/regular_match.json"
-    // data := utils.Read(path)
-    utils.R0(path)
+	path := "Input_samples/regular_match.json"
+	// data := utils.Read(path)
+	utils.ReadRequest(path)
 
-    // // Простое извлечение
-    // value, exists := data["events"]
-    // if exists {
-    //     fmt.Println("Найдено:", value)
-    // } else {
-    //     fmt.Println("Ключ не найден")
-    // }
+	// // Простое извлечение
+	// value, exists := data["events"]
+	// if exists {
+	//     fmt.Println("Найдено:", value)
+	// } else {
+	//     fmt.Println("Ключ не найден")
+	// }
 
-    // Теперь, data содержит разобранную структуру
-    // switch v := data.(type) {
-    // case map[string]interface{}:
-    //     fmt.Println("Объект JSON (словарь):")
-    //     fmt.Printf("%#vn", v)
-    // case []interface{}:
-    //     fmt.Println("Массив JSON:")
-    //     fmt.Printf("%#vn", v)
-    // default:
-    //     fmt.Printf("Неопознанный тип данных: %Tn", v)
-    // }
+	// Теперь, data содержит разобранную структуру
+	// switch v := data.(type) {
+	// case map[string]interface{}:
+	//     fmt.Println("Объект JSON (словарь):")
+	//     fmt.Printf("%#vn", v)
+	// case []interface{}:
+	//     fmt.Println("Массив JSON:")
+	//     fmt.Printf("%#vn", v)
+	// default:
+	//     fmt.Printf("Неопознанный тип данных: %Tn", v)
+	// }
 }
